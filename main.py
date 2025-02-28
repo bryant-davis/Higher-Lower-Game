442ff56e0f67e1e1dc7d3741093137555d8d1fc4
Bryant Davis <bryant@bryantdavis.com>
2025-02-28 16:12:05 -0600

    add basic logic for gameplay

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 # for demo see https://appbrewery.github.io/python-day14-demo/
-# Compare A: Ellen DeGeneres, a Comedian, from United States
 # outline:
 # function to populate compare a to compare b
 # input to accept user choice
 # function to compare choice against correct answer. Tracks how many right answers. If right calls function below. If wrong, displays final score and game ends
 # function to update compare fields
+
 import game_data
 import random
 
+score = 0
+
 def get_choices():
-    choice_index = random.randint(1, 50)
+    choice_index = random.randint(1, 49)
     choice = game_data.data[choice_index]
     choice_to_list = []
     choice_to_list.append(choice["name"])
@@ -26,30 +28,36 @@ def populate_against(name, desc, country):
     print("VERSUS")
     print(f'Against B: {name}, a {desc}, from {country}.')
 
+def evaluate(choice_1, choice_2, guess, score):
+    if choice_1[1] >= choice_2[1]:
+        winning_answer = "A"
+        winning_list = choice_1
+    else:
+        winning_answer = "B"
+        winning_list = choice_2
 
+    if guess == winning_answer:
+        choice_1 = winning_list
+        choice_2 = get_choices()
+        score += 1
+        gameplay(score)
+    else:
+        print("you lost!")
+        print(f'Final score: {score}')
+
+def gameplay(score):
+
+    choice_2 = get_choices()
+
+    populate_compare(choice_1[0], choice_1[2], choice_1[3])
+    populate_against(choice_2[0], choice_2[2], choice_2[3])
+    
+    guess = input("Who has more followers? Type 'A' or 'B':  ")
+    evaluate(choice_1, choice_2, guess, score)
 
 choice_1 = get_choices()
-choice_2 = get_choices()
-score = 0
+gameplay(score)
 
 
-populate_compare(choice_1[0], choice_1[2], choice_1[3])
-populate_against(choice_2[0], choice_2[2], choice_2[3])
 
-guess = input("Who has more followers? Type 'A' or 'B':  ")
-
-if choice_1[1] > choice_2[1]:
-    winning_answer = "A"
-    winning_list = choice_1
-else:
-    winning_answer = "B"
-    winning_list = choice_2
-
-if guess == winning_answer:
-    print('you won!')
-    choice_1 = winning_list
-    score += 1
-else:
-    print("you lost!")
-    print(f'Final score: {score}')
     
